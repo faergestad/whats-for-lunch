f6696b65061b985906c028258ba21c7b19cf5b39
faergestad <georg.f.persen@gmail.com>
2020-08-07 23:06:00 +0200

    Nope.. Revert

--- src/main/java/com/itera/faergestad/whatsforlunch/api/resources/LunchResource.java
+++ src/main/java/com/itera/faergestad/whatsforlunch/api/resources/LunchResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/lunch")
 @RequiredArgsConstructor
 public class LunchResource {
 
